e756a7b538e467fdb186558832d96e6c825b3bd1
Greg Leppert <greg@leppert.me>
2017-07-26 21:54:13 -0400

    Point to correct CSS destination for dev

--- gulpfile.js
+++ gulpfile.js
@@ -46,7 +46,7 @@ gulp.task('sass', function () {
             onError: browserSync.notify
         }))
         .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-        .pipe(gulp.dest('_site/css'))
+        .pipe(gulp.dest('_site/assets'))
         .pipe(browserSync.reload({stream:true}))
         .pipe(gulp.dest('assets'));
 });
